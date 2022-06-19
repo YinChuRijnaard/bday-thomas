6124cdf3676b0a3dceb29d50b0b8861ee5dec54e
Yin Chu <ycrijnaard@gmail.com>
2022-06-19 04:20:18 +0200

    Added questions for personal reference

--- pages/index.js
+++ pages/index.js
@@ -8,3 +8,7 @@ export default function Home() {
 
   return <>{isHisBirthday ? <HomeThomas /> : <HomeFriends />}</>;
 }
+
+// How do I make sure the /create route is ONLY accessible when logged in?
+// How do I make sure that signInWithRedirect actually routes to the create page. Atm, it only works when using signInWithPopup (note: signInWithPopup doesn't work on mobile, that's why)
+// What are the steps I need to take in order to (map and) display the messages in the HomeThomas component?
