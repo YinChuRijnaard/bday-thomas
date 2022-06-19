aaf9c91c383d7bd99943b80fbf31cbbb7e0f8964
Yin Chu <ycrijnaard@gmail.com>
2022-06-19 21:17:18 +0200

    Firebase test

--- components/HomeThomas.js
+++ components/HomeThomas.js
@@ -1,19 +1,20 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDocs } from "firebase/firestore";
 
-import { db, firestore, postToJSON } from "../firebase";
+import { db, colRef } from "../firebase";
 
-export async function getServerSideProps(context) {
-  const messagesQuery = firestore
-    .collectionGroup("bday-thomas")
-    .where("published", "==", true)
-    .orderBy("createdAt", "desc");
-
-  const messages = (await messagesQuery.get()).docs.map(postToJSON);
-
-  return { props: { messages } };
-
-  // Perhaps this is generally what I should be doing, based on existing code from the Next/Firebase course
-}
+getDocs(colRef).then((snapshot) => {
+  snapshot.docs.map((doc) => {
+    return (
+      <div>
+        <h1>{doc.data().message}</h1>
+        <br />
+        <p>
+          <strong>Door:</strong>&nbsp;{doc.data().author}
+        </p>
+      </div>
+    );
+  });
+});
 
 const HomeThomas = () => {
   return (
--- firebase.js
+++ firebase.js
@@ -1,26 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
-
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import "firebase/compat/storage";
-
-// Which of the above do I actually need?
+import { getFirestore, collection } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD91cemNSj2oQRHZH8qMkiquSZWG2x4D9w",
   authDomain: "bday-thomas.firebaseapp.com",
@@ -29,9 +15,6 @@ const firebaseConfig = {
   messagingSenderId: "123531827236",
   appId: "1:123531827236:web:d90cd907560f37583b60b1",
 };
-
-// Firestore exports
-export const firestore = firebase.firestore();
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -42,13 +25,3 @@ export const db = getFirestore(app);
 
 // Collection ref
 export const colRef = collection(db, "bday-messages");
-
-export function postToJSON(doc) {
-  const data = doc.data();
-  return {
-    ...data,
-    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
-    createdAt: data?.createdAt.toMillis() || 0,
-    updatedAt: data?.updatedAt.toMillis() || 0,
-  };
-}
