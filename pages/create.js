96e670a111d4b7433dab3fdbbf01294e750d3b64
Yin Chu <ycrijnaard@gmail.com>
2022-06-19 13:04:29 +0200

    Restricted access to create page if not logged in

--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Button, Heading, Textarea } from "@chakra-ui/react";
+import Link from "next/link";
+import { Button, Heading, Text, Textarea } from "@chakra-ui/react";
 
 import { AuthState } from "../AuthContext";
 
@@ -15,30 +16,48 @@ const WriteMessage = () => {
   };
 
   return (
-    <main className="flex h-screen w-screen flex-col bg-gray-300 text-black lg:justify-center">
-      <div className="p-4 lg:hidden"></div>
+    <>
+      {user ? (
+        <main className="flex h-screen w-screen flex-col bg-gray-300 text-black lg:justify-center">
+          <div className="p-4 lg:hidden"></div>
 
-      <div className="p-4 text-center lg:mx-48">
-        <Heading>Hey, {user.displayName}! 👋</Heading>
-      </div>
+          <div className="p-4 text-center lg:mx-48">
+            <Heading>Hey, {user.displayName}! 👋</Heading>
+          </div>
 
-      <div className="p-4"></div>
+          <div className="p-4"></div>
 
-      <div className="mx-4 lg:mx-48">
-        <Textarea
-          value={message}
-          onChange={handleChange}
-          placeholder="Schrijf je bericht..."
-          focusBorderColor="blue.500"
-        />
-      </div>
+          <div className="mx-4 lg:mx-48">
+            <Textarea
+              value={message}
+              onChange={handleChange}
+              placeholder="Schrijf je bericht..."
+              focusBorderColor="blue.500"
+            />
+          </div>
 
-      <div className="p-4"></div>
+          <div className="p-4"></div>
 
-      <div className="flex justify-center">
-        <Button colorScheme="blue">Plaats je bericht</Button>
-      </div>
-    </main>
+          <div className="flex justify-center">
+            <Button colorScheme="blue">Plaats je bericht</Button>
+          </div>
+        </main>
+      ) : (
+        <main className="flex h-screen w-screen flex-col items-center justify-center bg-gray-300 text-black lg:justify-center">
+          <div className="p-4 text-center lg:mx-48">
+            <Text>
+              Oh oh... je moet ingelogd zijn om deze pagina te kunnen bekijken.
+            </Text>
+
+            <div className="p-4"></div>
+
+            <Link href="/">
+              <Button colorScheme="blue">Ga terug naar de homepagina</Button>
+            </Link>
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
