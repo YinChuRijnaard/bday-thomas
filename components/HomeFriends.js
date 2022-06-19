0c0d448b2de693b854dda421d375422772ceeaba
Yin Chu <ycrijnaard@gmail.com>
2022-06-19 18:28:01 +0200

    Basic style changes

--- components/HomeFriends.js
+++ components/HomeFriends.js
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
-import { Button, Heading, Text } from "@chakra-ui/react";
+
+import { Button, Heading, Link, Text } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+
+import Google from "../components/icons/Google";
 
 const HomeFriends = () => {
   const router = useRouter();
   const auth = getAuth();
-  // const provider = new GoogleAuthProvider(); // No longer necessary
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
   useEffect(() => {
@@ -20,48 +23,34 @@ const HomeFriends = () => {
     }
   }, [user]);
 
-  // signInWithPopup(auth, provider)
-  //   .then((result) => {
-  //     const credential = GoogleAuthProvider.credentialFromResult(result);
-  //     // @ts-ignore
-  //     const token = credential.accessToken;
-  //     const user = result.user;
-  //     router.push("/create");
-  //     toast.success("Je bent ingelogd!");
-  //   })
-  //   .catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     const email = error.customData.email;
-  //     const credential = GoogleAuthProvider.credentialFromError(error);
-  //     toast.error(error);
-  //   });
-
   return (
-    <main className="flex h-screen w-screen flex-col justify-center bg-[url('/bg.jpeg')] bg-cover bg-center bg-no-repeat text-white lg:bg-top">
-      <div className="bg-gray-500/50 p-4 text-center lg:mx-48">
-        <Heading size="2xl">
-          Schrijf een leuk bericht voor Thomas z&apos;n 26<sup>e</sup>{" "}
-          verjaardag! 🥳
+    <main className="flex h-screen w-screen flex-col justify-center bg-[url('/bg.jpeg')] bg-cover bg-center bg-no-repeat lg:bg-top">
+      <div className="bg-gray-500/25 p-4 text-center text-white lg:mx-48">
+        <Heading size="3xl">
+          Schrijf een leuk bericht voor Thomas z&apos;n verjaardag! 🥳
         </Heading>
 
         <div className="p-4"></div>
-        <div className="p-4"></div>
-        <div className="p-4"></div>
 
-        <Text>
-          Het is bijna Thomas z&apos;n verjaardag! 🎉 Daarom leek het mij, Yin
-          Chu, een leuk idee om een kleine app te maken waar vrienden en familie
-          een berichtje achter kunnen laten. Lijkt je dat leuk? Log dan snel in
-          met je Google account! 😊
+        <Text fontSize="xl">
+          Het is bijna Thomas z&apos;n verjaardag! 🎉 Daarom leek het mij,{" "}
+          <Link href="https://www.instagram.com/yinnn.chu/" isExternal>
+            Yin Chu &#40;
+            <ExternalLinkIcon mx="2px" />
+            &#41;
+          </Link>
+          , een leuk idee om een kleine app te maken waar vrienden en familie
+          een berichtje achter kunnen laten. Lijkt je dat leuk? Log dan snel in!
+          😊
         </Text>
       </div>
 
       <div className="p-4"></div>
 
       <div className="flex justify-center">
-        <Button colorScheme="blue" onClick={() => signInWithGoogle()}>
-          Log in en schrijf je bericht 🖊️
+        <Button colorScheme="gray" onClick={() => signInWithGoogle()}>
+          <Google />
+          Inloggen met Google
         </Button>
       </div>
     </main>
--- pages/create.js
+++ pages/create.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import Link from "next/link";
+import toast from "react-hot-toast";
 import { addDoc, collection } from "firebase/firestore";
 import { Button, Heading, Text, Textarea } from "@chakra-ui/react";
+
 import { AuthState } from "../AuthContext";
 import { db, colRef } from "../firebase";
 
 const WriteMessage = () => {
   const [message, setMessage] = useState("");
+  const [disabled, setDisabled] = useState(false);
   const { user } = AuthState();
 
   // console.log(user);
@@ -17,51 +20,65 @@ const WriteMessage = () => {
   };
 
   const handleClick = () => {
-    console.log(message);
-
-    // addDoc(collection(colRef, "bday-messages"), {
-    //   author: user.displayName,
-    //   message: message,
-    // });
-
     addDoc(colRef, {
       author: user.displayName,
       message: message,
     });
+
+    setMessage("");
+    setDisabled(!disabled);
+    toast.success("Bericht geplaatst!");
   };
 
   return (
     <>
       {user ? (
-        <main className="flex h-screen w-screen flex-col bg-gray-300 text-black lg:justify-center">
+        <main className="flex h-screen w-screen flex-col bg-gradient-to-b from-blue-500 via-red-500 to-amber-500 text-white lg:justify-center">
           <div className="p-4 lg:hidden"></div>
 
-          <div className="p-4 text-center lg:mx-48">
+          <div className="p-4 text-center lg:mx-96">
             <Heading>Hey, {user.displayName}! 👋</Heading>
           </div>
 
           <div className="p-4"></div>
 
-          <div className="mx-4 lg:mx-48">
+          <div className="mx-4 lg:mx-96">
             <Textarea
               value={message}
               onChange={handleChange}
               placeholder="Schrijf je bericht..."
-              focusBorderColor="blue.500"
+              focusBorderColor="black"
+              disabled={disabled}
             />
           </div>
 
           <div className="p-4"></div>
 
-          <div className="flex justify-center">
-            <Button colorScheme="blue" onClick={handleClick}>
-              Plaats je bericht
+          <div className="mx-4 flex justify-center text-black lg:mx-96">
+            <Button
+              colorScheme="gray"
+              onClick={handleClick}
+              disabled={disabled}
+            >
+              Plaatsen
             </Button>
+            {/* <Button colorScheme="gray">Bewerken</Button> */}
           </div>
+
+          <div className="p-4"></div>
+
+          {disabled && (
+            <div className="p-4 text-center lg:mx-96">
+              <Text>
+                <strong>Bedankt!</strong> <br /> Je bericht is geplaatst. Bezoek
+                deze website weer op 22 juli om alle andere berichten te lezen.
+              </Text>
+            </div>
+          )}
         </main>
       ) : (
-        <main className="flex h-screen w-screen flex-col items-center justify-center bg-gray-300 text-black lg:justify-center">
-          <div className="p-4 text-center lg:mx-48">
+        <main className="flex h-screen w-screen flex-col items-center justify-center lg:justify-center">
+          <div className="p-4 text-center lg:mx-96">
             <Text>
               Oh oh... je moet ingelogd zijn om deze pagina te kunnen bekijken.
             </Text>
@@ -69,7 +86,7 @@ const WriteMessage = () => {
             <div className="p-4"></div>
 
             <Link href="/">
-              <Button colorScheme="blue">Ga terug naar de homepagina</Button>
+              <Button colorScheme="gray">Ga terug naar de homepagina</Button>
             </Link>
           </div>
         </main>
