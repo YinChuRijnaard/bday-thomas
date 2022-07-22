7816a2a9cedbbb1d5058bdf725d6b92ab63a14ee
Yin Chu <ycrijnaard@gmail.com>
2022-07-22 07:56:49 +0200

    Permanently display messages

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const isHisBirthday = useCheckBirthday();
 
   // @ts-ignore
-  return <>{isHisBirthday ? <HomeThomas /> : <HomeFriends />}</>;
+  return <>{!isHisBirthday ? <HomeThomas /> : <HomeFriends />}</>;
 }
 
 // Organise imports
